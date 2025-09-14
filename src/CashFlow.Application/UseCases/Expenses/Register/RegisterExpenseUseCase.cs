95ac201edeb8774588e0e0543fec9e310380ed8f
Miguel Delgado <delgdevoleper@gmail.com>
2025-09-14 17:40:44 -0300

    feat(RegisterExpense): adiciona fluent validation e exceptions personalizadas

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,6 +1,6 @@
-﻿using CashFlow.Communication.Enums;
-using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionsBase;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
 
@@ -23,16 +23,15 @@ public class RegisterExpenseUseCase
 
     private void Validate(RequestRegisterExpenseJson request)
     {
-        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
-        if (titleIsEmpty) throw new ArgumentException("The title is required");
+        var validator = new RegisterExpenseValidator();
+        var result = validator.Validate(request);
 
-        if (request.Amount <= 0) throw new ArgumentException("The value must be grater than 0");
+        if (!result.IsValid)
+        {
+            var errorMessages = result.Errors.Select(err => err.ErrorMessage).ToList();
+            throw new ErrorOnValidationException(errorMessages);
+        }
 
-        var dateCompareResult = DateTime.Compare(request.Date, DateTime.UtcNow);
-        if(dateCompareResult > 0) throw new ArgumentException("The date must be in the present");
-
-        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
-        if (!paymentTypeIsValid) throw new ArgumentException("Payment Type is not valid");
     }
 }
 
---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
@@ -0,0 +1,19 @@
+﻿using CashFlow.Communication.Requests;
+using FluentValidation;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+
+public class RegisterExpenseValidator : AbstractValidator<RequestRegisterExpenseJson>
+{
+    public RegisterExpenseValidator()
+    {
+        RuleFor(expense => expense.Title).NotEmpty().WithMessage("The title is required");
+
+        RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage("The value must be greater than 0");
+
+        RuleFor(expense => expense.Date).LessThanOrEqualTo(DateTime.UtcNow).WithMessage("The date must be in the present");
+
+        RuleFor(expense => expense.PaymentType).IsInEnum().WithMessage("The date must be in the present");
+    }
+
+}
