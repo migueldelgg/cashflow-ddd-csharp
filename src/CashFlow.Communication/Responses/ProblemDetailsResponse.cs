a7c3753b8f8c5381fe1473bed9e0cbf4e489348e
Miguel Delgado <delgdevoleper@gmail.com>
2025-09-14 18:38:32 -0300

    feat(exceptions): adiciona filtro de exceptions

---
+++ src/CashFlow.Communication/Responses/ProblemDetailsResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace CashFlow.Communication.Responses;
+
+public class ProblemDetailsResponse
+{
+    public List<string> ErrorMessages { get; init; }
+
+    public ProblemDetailsResponse(string errorMessage)
+    {
+        ErrorMessages = [errorMessage];
+    }
+
+    public ProblemDetailsResponse(List<string> errorMessages)
+    {
+        ErrorMessages = errorMessages;
+    }
+}
--- src/CashFlowAPI/Controllers/ExpensesController.cs
+++ src/CashFlowAPI/Controllers/ExpensesController.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using CashFlow.Communication.Requests;
-using Microsoft.AspNetCore.Mvc.Formatters;
 using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionsBase;
 
 namespace CashFlowAPI.Controllers;
 
@@ -15,7 +16,7 @@ public class ExpensesController : ControllerBase
     {
         var useCase = new RegisterExpenseUseCase();
         var response = useCase.Execute(request);
-
+        
         return Created(string.Empty, response);
     }
 }
