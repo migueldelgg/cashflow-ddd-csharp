20c9149e7a09147f2cdb4cb44d147d8e46dca3e0
Miguel Delgado <delgdevoleper@gmail.com>
2025-09-14 12:50:12 -0300

    feat(Application,Communication,Domain,Exception,Infra,Api): adiciona demais projetos e adiciona endpoint de registrar gastos

---
+++ src/CashFlowAPI/Controllers/ExpensesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CashFlow.Communication.Requests;
+
+namespace CashFlowAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : ControllerBase
+{
+
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
+    {
+
+        return Created();
+    }
+}
+
